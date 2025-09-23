951901c3e64b91d787a1eaedf1a60f6efb4a5318
Denzel Jacob <70809285+DenzelJacob@users.noreply.github.com>
2025-09-22 20:59:34 -0400

    Subtypes and concatenation of strings
    Be very specific when allocating char's

--- PLSQL_basics.sql
+++ PLSQL_basics.sql
@@ -1,5 +1,21 @@
+
+/*
 DECLARE 
+   --name type := value;
    message  varchar2(20):= 'Hello, World!'; 
 BEGIN 
    dbms_output.put_line(message); 
+END; 
+
+*/
+DECLARE 
+   -- subtype can work as an alias for a datatype
+   SUBTYPE name IS char(7); -- notice how the amount of chars allocated  controls the spacing in the output
+   SUBTYPE message IS varchar2(100); 
+   salutation name; 
+   greetings message; 
+BEGIN 
+   salutation := 'Reader '; 
+   greetings := 'Welcome to the World of PL/SQL'; 
+   dbms_output.put_line('Hello ' || salutation || greetings); 
 END; 
