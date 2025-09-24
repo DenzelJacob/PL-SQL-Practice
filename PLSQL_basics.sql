8c216cf13662f6f508dc8d53338ace49d719d6b9
Denzel Jacob <70809285+DenzelJacob@users.noreply.github.com>
2025-09-24 13:04:51 -0400

    operations

--- PLSQL_basics.sql
+++ PLSQL_basics.sql
@@ -7,7 +7,6 @@ BEGIN
    dbms_output.put_line(message); 
 END; 
 
-*/
 DECLARE 
    -- subtype can work as an alias for a datatype
    SUBTYPE name IS char(7); -- notice how the amount of chars allocated  controls the spacing in the output
@@ -17,5 +16,17 @@ DECLARE
 BEGIN 
    salutation := 'Reader '; 
    greetings := 'Welcome to the World of PL/SQL'; 
-   dbms_output.put_line('Hello ' || salutation || greetings); 
+   dbms_output.put_line('Hello ' || salutation || greetings); -- concatenation operator is ||
+END; 
+*/
+DECLARE 
+   a integer := 10; 
+   b integer := 20; 
+   c integer; 
+   f real; 
+BEGIN 
+   c := a + b; 
+   dbms_output.put_line('Value of c: ' || c); 
+   f := 70.0/3.0; 
+   dbms_output.put_line('Value of f: ' || f ); 
 END; 
